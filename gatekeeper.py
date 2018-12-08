7549011aa2ed38403e702b668337d0f7338c2684
Ryan Zhang <ryanzhang0112@gmail.com>
2018-12-08 13:56:04 -0500

    Load keys

--- gatekeeper.py
+++ gatekeeper.py
@@ -2,21 +2,39 @@ import os
 import glob
 import keyrunner
 from console import *
+import threading
 
 
 def main():
+    t = threading.Thread(target=load_key)
+    t.start()
+
     with open('splash', 'r') as f:
         Console.print(f.read(), Colors.PURPLE_BOLD_BRIGHT)
 
 
-def load_key(user):
-    keys = glob.glob("/keybase/public/"+user)
+def load_key():
+    global waiting
+
+    keys = glob.glob("/keybase/public/"+user + "/*.key")
+    current_keys = set(keyrunner.list_keys())
+    new_keys = set()
 
     for key in keys:
         key = open(key).read()
+        new_keys.add(key)
 
-        print (key)
+        if key not in current_keys:
+            keyrunner.add_key(key)
+
+    for k in current_keys - new_keys:
+        keyrunner.revoke_key(k)
+
+    waiting = False
 
 if __name__ == '__main__':
     main()
+
+    waiting = True
+    user = "henrytwo"
 #hello
