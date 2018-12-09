81cadca444dd87b8fe53e160adcc48c607b34dd5
syed-safwaan <syed.safwaan@gmail.com>
2018-12-08 21:03:38 -0500

    Updated log method

--- dataparsing.py
+++ dataparsing.py
@@ -10,8 +10,19 @@ data_colors = {
 }
 
 
-def log(key: str, timestamp: str, type: str, action: str):
-    pass
+def log(key: str, timestamp: str, typ: str, action: str):
+    with open('log.json', mode='r+', encoding='utf-8') as f:
+        data = json.load(f)
+        data.append({
+            "key": key,
+            "timestamp": timestamp,
+            "type": typ,
+            "action": action
+        })
+
+        f.seek(0)
+        json.dump(data[:min(len(data), 50)], f, indent=4)
+        f.truncate()
 
 
 def print_log():
@@ -22,8 +33,16 @@ def print_log():
     Console.print(f'║ {"Key":<50} ║ {"Timestamp":15} ║ {"Type":12} ║ {"Message":100} ║', Colors.BLACK_BOLD)
     Console.print(f'╠═{"═" * 50}═╬═{"═" * 15}═╬═{"═" * 12}═╬═{"═" * 100}═╣', Colors.BLACK_BOLD)
     for t in data:
-        Console.print(f'║ {t["key"]:<50} ║ {t["timestamp"].upper():15} ║ {t["type"].upper():12} ║ {t["action"]:100} ║', data_colors[t['type']])
+        Console.print(f'║ {t["key"]:<50} ║ {t["timestamp"].upper():15} ║ {t["type"].upper():12} ║ {t["action"]:100} ║',
+                      data_colors[t['type']])
     Console.print(f'╚═{"═" * 50}═╩═{"═" * 15}═╩═{"═" * 12}═╩═{"═" * 100}═╝', Colors.BLACK_BOLD)
     "╚═══╩═══╩═══╝"
 
-print_log()
+
+if __name__ == '__main__':
+    log('one', '1', 'lock/unlock', 'heh')
+    log('two', '2', 'add-key', 'heh')
+    log('three', '3', 'revoke-key', 'heh')
+    log('four', '4', 'login', 'heh')
+    log('five', '5', 'kick', 'heh')
+    print_log()
