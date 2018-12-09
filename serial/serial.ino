64d2edbf05edf29e54c7a8c8629d6199db1d1495
Andrew Gao <aggaodrew@gmail.com>
2018-12-09 03:46:33 -0500

    Add files via upload

--- serial/serial.ino
+++ serial/serial.ino
@@ -8,6 +8,9 @@ Servo servo;
 int lockLED = 2;
 int ulockLED = 3;
 String str = "";
+int button = 12;
+int buttonState = 0;
+boolean overRide = false;
 
 //---------------- help list -----------------------------------------
 void printHelp(void){
@@ -23,6 +26,7 @@ void setup(){
   pinMode(lockLED, OUTPUT);
   pinMode(ulockLED, OUTPUT);
   servo.attach(13);
+  pinMode(button, INPUT);
   lcd.begin(16,2);
   lcd.setCursor(0,0);
   lcd.print("Hello there!");
@@ -34,23 +38,58 @@ void setup(){
 
 //--------------- loop ----------------------------------------------- 
 void loop(){
+  buttonState = digitalRead(button);
+  
+  if (buttonState == HIGH){
+      if (overRide == false){
+        digitalWrite(lockLED, HIGH);
+        digitalWrite(ulockLED, LOW);
+        lcd.setCursor(0,0);
+        lcd.print("Device Status:");
+        lcd.setCursor(0,1);
+        lcd.print("Locked");
+        servo.write(0);
+        delay(1000);
+        lcd.clear();
+      }
+  }
+  else if (buttonState == LOW){
+      digitalWrite(ulockLED, HIGH);
+      digitalWrite(lockLED, LOW);
+      lcd.setCursor(0,0);
+      lcd.print("Device Status:");
+      lcd.setCursor(0,1);
+      lcd.print("Unlocked");
+      servo.write(180);
+      delay(1000);
+      lcd.clear();
+  }    
+  
   if (Serial.available() > 0){
     str = Serial.readStringUntil('\n');
     Serial.flush();
 
     if (str == "lock"){
+      overRide = false;
       digitalWrite(lockLED, HIGH);
       digitalWrite(ulockLED, LOW);
-      lcd.print("locked");
+      lcd.setCursor(0,0);
+      lcd.print("Device Status:");
+      lcd.setCursor(0,1);
+      lcd.print("Locked");
       servo.write(0);
       delay(1000);
       lcd.clear();
     }
     
     else if (str == "unlock"){
+      overRide = true;
       digitalWrite(ulockLED, HIGH);
       digitalWrite(lockLED, LOW);
-      lcd.print("unlocked");
+      lcd.setCursor(0,0);
+      lcd.print("Device Status:");
+      lcd.setCursor(0,1);
+      lcd.print("Unlocked");
       servo.write(180);
       delay(1000);
       lcd.clear();
